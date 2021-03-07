a6c6262e4c0be4923c7b81f12115e7cacc347abe
Aditi Hiremath <ahiremath@utexas.edu>
2021-03-07 10:21:27 -0600

    Initial Commit

---
+++ Billify/View Controllers/SignUpViewController.swift
@@ -0,0 +1,52 @@
+//
+//  SignUpViewController.swift
+//  Billify
+//
+//  Created by Aditi on 3/6/21.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    @IBOutlet weak var firstNameTextField: UITextField!
+    
+    @IBOutlet weak var lastNameTextField: UITextField!
+    
+    @IBOutlet weak var birthdayPicker: UIDatePicker!
+    
+    @IBOutlet weak var insuranceTextField: UITextField!
+    
+    @IBOutlet weak var insurNumTextField: UITextField!
+    
+    @IBOutlet weak var groupNumTextField: UITextField!
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var signUpButton: UITextField!
+    
+    @IBOutlet weak var errorLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    @IBAction func signUpTapped(_ sender: Any) {
+    }
+    
+}
